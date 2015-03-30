5fb16ca28b8420366d015859a900cf90a9f090a8
brockuniera <craigbrockunier@wireless-10-148-147-49.public.utexas.edu>
2015-03-30 03:00:49 -0500

    Finish timer and auto switching

---
+++ Assets/Scripts/General Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public struct Timer{
+
+	//The time to wait for
+	private float _goalTime;
+
+	//returns true if enough time passed
+	public bool isDone {
+		get{ return Time.time >= _goalTime; }
+	}
+
+	//gets current difference in time between now and goal
+	//- is before goal; + is after goal
+	public float getTime {
+		get{ return Time.time - _goalTime; }
+	}
+
+	//Sets how long the timer should time for
+	public void SetTimer(float length){
+		_goalTime = length + Time.time;
+	}
+
+}
+
---
+++ Assets/Scripts/Logic/FPSChanger.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Responsible for giving player control to a fps prefab
+public class FPSChanger : MonoBehaviour{
+
+	//
+	//Private data
+	//
+
+	//List of our FPSControllers
+	//
+	//Populated by users
+	public GameObject[] FPSControllers;
+	//index of current active go
+	private int m_currIndex;
+
+	//Timer for switching FPSController
+	//
+	private Timer m_switchTimer;
+	public float SwitchTime;
+
+	//
+	//Unity callbacks
+	//
+
+	void Awake(){
+		//Make sure we're cool
+		if(FPSControllers == null){
+			Debug.LogError("FPSControllers is empty!!");
+			Application.Quit();
+		}
+
+		//init index
+		m_currIndex = 0;
+
+		//Disable all but one in scene
+		foreach(GameObject go in FPSControllers){
+			go.SetActive(false);
+		}
+		FPSControllers[0].SetActive(true);
+	}
+	
+	//Update is called once per frame
+	void Update(){
+		//Timer to switch controlled guy
+		if(m_switchTimer.isDone){
+			CycleFPSController();
+			m_switchTimer.SetTimer(SwitchTime);
+		}
+	}
+
+	//
+	//Public Methods
+	//
+
+	//Sets currently selected FPSController to next in list
+	public void CycleFPSController(){
+		//Disable current FSPCont
+		FPSControllers[m_currIndex++].SetActive(false);
+
+		//loop back around list if needed
+		if(m_currIndex == FPSControllers.Length)
+			m_currIndex = 0;
+
+		//Enable this guy
+		FPSControllers[m_currIndex].SetActive(true);
+	}
+
+}
+
