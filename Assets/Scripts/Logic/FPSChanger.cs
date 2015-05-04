da5ce54eab1044a408937f9b1228f840223a1d4e
brockuniera <brockuniera@utexas.edu>
2015-05-04 16:23:34 -0500

    Update logger, remove sub

--- Assets/Scripts/Logic/FPSChanger.cs
+++ Assets/Scripts/Logic/FPSChanger.cs
@@ -4,31 +4,41 @@ using System.Collections;
 //Responsible for giving player control to a fps prefab
 public class FPSChanger : MonoBehaviour{
 
+
+	//Parralel lists of our FPSControllers and Enviroments
+	//
+	public GameObject[] Worlds;
+	public GameObject[] FPSControllers;
+
+	//Index of current active go
+	private int m_currIndex;
+
 	//
 	//Private data
 	//
-
-	//List of our FPSControllers
-	//
-	//Populated by users
-	public GameObject[] FPSControllers;
-	//index of current active go
-	private int m_currIndex;
 
 	//Timer for switching FPSController
 	//
 	private Timer m_switchTimer;
 	public float SwitchTime;
 
+	//Ref to our logger
+	private Logger logger;
+
 	//
 	//Unity callbacks
 	//
 
-	void Awake(){
+	//Sets everything up
+	void Start(){
 		//Make sure we're cool
 		//
 		if(FPSControllers == null){
-			Debug.LogError("FPSControllers is empty!!");
+			Debug.LogError("List of FPSControllers is empty!!");
+			Application.Quit();
+		}
+		if(Worlds == null){
+			Debug.LogError("List of Worlds is empty!!");
 			Application.Quit();
 		}
 
@@ -37,6 +47,7 @@ public class FPSChanger : MonoBehaviour{
 		m_currIndex = 0;
 
 		//Disable all but one in scene
+		//
 		foreach(GameObject go in FPSControllers){
 			go.SetActive(false);
 		}
@@ -45,13 +56,26 @@ public class FPSChanger : MonoBehaviour{
 		//Setup timer
 		//
 		m_switchTimer.SetTimer(SwitchTime);
+
+		//Setup logger
+		//
+		logger = GetComponent<Logger>();
+		if(logger == null){
+			Debug.LogError("Couldn't find logger!!");
+			return;
+		}
+		logger.StartTrial("FPV", Vector2.zero, "--", FPSControllers[m_currIndex], Worlds[m_currIndex].transform.position);
 	}
 	
-	//Update is called once per frame
+	//Cycles to the next fps controller
 	void Update(){
 		//Timer to switch controlled guy
 		if(m_switchTimer.isDone){
+			//TODO Add fade to black
+
+			//Update game
 			CycleFPSController();
+			//Reset timer
 			m_switchTimer.SetTimer(SwitchTime);
 		}
 	}
@@ -62,8 +86,12 @@ public class FPSChanger : MonoBehaviour{
 
 	//Sets currently selected FPSController to next in list
 	public void CycleFPSController(){
-		//Disable current FSPCont
-		FPSControllers[m_currIndex++].SetActive(false);
+		//Disable current
+		FPSControllers[m_currIndex].SetActive(false);
+		//Worlds[m_currIndex].SetActive(false);
+
+		//Increment index
+		m_currIndex++;
 
 		//loop back around list if needed
 		if(m_currIndex == FPSControllers.Length)
@@ -71,7 +99,11 @@ public class FPSChanger : MonoBehaviour{
 
 		//Enable this guy
 		FPSControllers[m_currIndex].SetActive(true);
-	}
+		//Worlds[m_currIndex].SetActive(true);
 
+		//Update logger
+		logger.EndTrial();
+		logger.StartTrial("FPV", Vector2.zero, "--", FPSControllers[m_currIndex], Worlds[m_currIndex].transform.position);
+	}
 }
 
--- Assets/Scripts/Serializing/Logger.cs
+++ Assets/Scripts/Serializing/Logger.cs
@@ -4,18 +4,54 @@ using System.Xml;
 
 //Logs to a file every fixed update
 //TODO
-//	Use a queued thread system:
-//		push updates to the thread,
-//		it flushes every so and so time
+//	Use a queue system:
+//		push updates to some master,
+//		it flushes every now and then
 //	Use xml
 //	Integrate with existing matlab workflow in Preston_Nav/tools/
 public class Logger : MonoBehaviour {
+	//
+	//Highest level tag settings
+	//
+	
+	//Which run is this?
+	public int RunNumber = -1;
 
-	//Path to output
+	//Name of subject
+	public string SubjectName = "name not assigned";
+	
+	//Version number
+	public string VersionString = "1.0";
+
+	//
+	//Other settings
+	//
+
+	//File to output to
 	public string XmlLogOutput = "temp.xml";
 
 	//Log timer interval
 	public float LogTimeInterval = .8f;
+
+	//
+	//Private members
+	//
+
+	//Logging relevant state
+	//
+
+	//What GO should we log? This is the player and
+	//is changed by the FPSChooser script
+	private GameObject gameObjectToLog;
+
+	//The goal the player is trying to move towards
+	private Vector2 goalDestination;
+	
+	//The origin of each world
+	private Vector2 relativeOrigin;
+
+	//Other private state
+	//
 
 	//XmlWriter
 	private XmlWriter m_writer;
@@ -23,20 +59,64 @@ public class Logger : MonoBehaviour {
 	//Timer
 	private Timer logTimer;
 
+	//Are we currently inside a trial element?
+	private bool inTrial = false;
+
+	//
+	//Public methods
+	//
+
+	//Called to start the recording of a trial
+	public void StartTrial(string camera, Vector2 destination, string landmark, GameObject trackme, Vector2 relOrigin){
+
+		//Setup local state
+		//
+		gameObjectToLog = trackme;
+		goalDestination = destination;
+		relativeOrigin = relOrigin;
+
+		//Write a trial element
+		//
+		m_writer.WriteStartElement("trial");
+
+		m_writer.WriteAttributeString("camera", camera);
+		m_writer.WriteAttributeString("goalx", (goalDestination.x - relativeOrigin.x).ToString());
+		m_writer.WriteAttributeString("goaly", (goalDestination.y - relativeOrigin.y).ToString());
+		m_writer.WriteAttributeString("landmark", landmark);
+		m_writer.WriteAttributeString("pose", gameObjectToLog.transform.rotation.eulerAngles.y.ToString());
+		m_writer.WriteAttributeString("startx", (gameObjectToLog.transform.position.x - relativeOrigin.x).ToString());
+		m_writer.WriteAttributeString("starty", (gameObjectToLog.transform.position.y - relativeOrigin.y).ToString());
+
+		//Setup timer; other state
+		//
+		inTrial = true;
+		logTimer.SetTimer(LogTimeInterval);
+	}
+
+	public void EndTrial(){
+		m_writer.WriteEndElement();
+		inTrial = false;
+	}
+
 	//
 	//Helper methods
 	//
 
-	//Output current state to Xml
-	private void WriteData(){
-		//Writing this tick...
-		m_writer.WriteStartElement("tick");
+	//Log the state of the current frame
+	private void WriteFrame(){
+		//Writing this frame...
+		m_writer.WriteStartElement("frame");
 
-		m_writer.WriteElementString("pos", transform.position.ToString());
-		m_writer.WriteElementString("rot", transform.rotation.ToString());
-		m_writer.WriteElementString("time", Time.time.ToString());
+		//This is our relevant data:
+		//
+		Transform t = gameObjectToLog.transform;
+		m_writer.WriteAttributeString("distance", Vector2.Distance(t.position, goalDestination).ToString());
+		m_writer.WriteAttributeString("pose", t.rotation.eulerAngles.y.ToString());
+		m_writer.WriteAttributeString("timestamp", Time.time.ToString());
+		m_writer.WriteAttributeString("x", (t.position.x - relativeOrigin.x).ToString());
+		m_writer.WriteAttributeString("y", (t.position.y - relativeOrigin.y).ToString());
 
-		//...done!
+		//Done!
 		m_writer.WriteEndElement();
 	}
 
@@ -44,7 +124,7 @@ public class Logger : MonoBehaviour {
 	//Unity callbacks
 	//
 
-	void Start(){
+	void Awake(){
 		//Setup timer
 		logTimer = new Timer();
 
@@ -52,27 +132,38 @@ public class Logger : MonoBehaviour {
 		//
 
 		//Setup XmlWriter with indenting enabled (uses hot C# syntax for Object Initializer)
-		//TODO This outputs bad xml, for some reason!!! TODO
 		//TODO Try/catch/finally
 		m_writer = XmlWriter.Create(XmlLogOutput, new XmlWriterSettings(){Indent = true});
 
 		//Start our document
 		m_writer.WriteStartDocument();
-		m_writer.WriteStartElement("Test output");
+
+		//Add some high level information
+		m_writer.WriteStartElement("run");
+		m_writer.WriteAttributeString("number", RunNumber.ToString());
+		m_writer.WriteAttributeString("subject", SubjectName);
+		m_writer.WriteAttributeString("version", VersionString);
 	}
 
 	//Log whatever
 	void Update(){
-		if(logTimer.isDone){
-			WriteData();
+		//We write based on this timer, not based on framerate
+		if(inTrial && logTimer.isDone){
+			WriteFrame();
 			logTimer.SetTimer(LogTimeInterval);
 		}
 	}
 
 	//TODO exceptions/using statements
+	//Close our file and that kind of thing
 	void OnDestroy(){
-		//Elements
+		if(inTrial)
+			EndTrial();
+
+		//End high level information
 		m_writer.WriteEndElement();
+
+		//End our document
 		m_writer.WriteEndDocument();
 
 		//Close file
