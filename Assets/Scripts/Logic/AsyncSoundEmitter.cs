35186fd8f99b55c1f8a017ec3a3be8422776dd1c
Neal Morton <mortonne@gmail.com>
2017-06-29 14:16:51 -0500

    remove audio and logging

--- Assets/Scripts/Logic/AsyncSoundEmitter.cs
+++ Assets/Scripts/Logic/AsyncSoundEmitter.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(AudioSource))]
+//[RequireComponent(typeof(AudioSource))]
 public class AsyncSoundEmitter : MonoBehaviour {
 
     // Logger, and related variables
@@ -9,13 +9,13 @@ public class AsyncSoundEmitter : MonoBehaviour {
     private readonly string configFileName = "soundconfig.txt";
     private readonly string outputFileName = "soundoutput.xml";
 
-    private AudioSource myaudio;
+    //private AudioSource myaudio;
 
 	public Transform SquareManager;
 
     public void Start(){
-        logger = new SoundLogger(outputFileName);
-        myaudio = GetComponent<AudioSource>();
+        //logger = new SoundLogger(outputFileName);
+        //myaudio = GetComponent<AudioSource>();
 
         SoundConfigInfo info = SoundConfigReader.CreateConfig(configFileName);
 		print(string.Format("lo:{0}, hi:{1}, dur:{2}", info.lowTime, info.hiTime, info.durTime));
@@ -26,15 +26,14 @@ public class AsyncSoundEmitter : MonoBehaviour {
 	public IEnumerator PlaySounds(float low, float high, float dur){
         // Play sounds forever, starting immediatly, delayed uniform time between low and high
         while(true){
-            myaudio.Play();
-            logger.Log(Time.time.ToString());
+            //myaudio.Play();
 			SquareManager.SendMessage ("Blink", dur);
             yield return new WaitForSeconds(RandomHelper.Uniform(low, high));
         }
     }
     
-    void OnDestroy(){
-        logger.Dispose();
-    }
+    //void OnDestroy(){
+    //    logger.Dispose();
+    //}
 }
 
