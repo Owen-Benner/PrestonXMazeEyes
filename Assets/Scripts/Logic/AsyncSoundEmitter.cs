4f20c85e1eedd9e4f5d5c2a85d362cdfa0a62062
Neal Morton <mortonne@gmail.com>
2017-06-29 15:55:53 -0500

    remove old logging and audio playing code

--- Assets/Scripts/Logic/AsyncSoundEmitter.cs
+++ Assets/Scripts/Logic/AsyncSoundEmitter.cs
@@ -4,36 +4,23 @@ using System.Collections;
 //[RequireComponent(typeof(AudioSource))]
 public class AsyncSoundEmitter : MonoBehaviour {
 
-    // Logger, and related variables
-    private SoundLogger logger;
+    // Config file
     private readonly string configFileName = "soundconfig.txt";
-    private readonly string outputFileName = "soundoutput.xml";
-
-    //private AudioSource myaudio;
 
 	public Transform SquareManager;
 
     public void Start(){
-        //logger = new SoundLogger(outputFileName);
-        //myaudio = GetComponent<AudioSource>();
-
         SoundConfigInfo info = SoundConfigReader.CreateConfig(configFileName);
 		print(string.Format("lo:{0}, hi:{1}, dur:{2}", info.lowTime, info.hiTime, info.durTime));
-
 		StartCoroutine(PlaySounds(info.lowTime, info.hiTime, info.durTime));
     }
 
 	public IEnumerator PlaySounds(float low, float high, float dur){
         // Play sounds forever, starting immediatly, delayed uniform time between low and high
         while(true){
-            //myaudio.Play();
 			SquareManager.SendMessage ("Blink", dur);
             yield return new WaitForSeconds(RandomHelper.Uniform(low, high));
         }
     }
-    
-    //void OnDestroy(){
-    //    logger.Dispose();
-    //}
 }
 
