4636909035d384017e022cadb78dd4d78fbca43e
Owen-Benner <owen.p.benner@gmail.com>
2019-02-18 13:35:50 -0600

    new file:   Assets/New Terrain 1.asset 	new file:   Assets/New Terrain 1.asset.meta 	new file:   Assets/New Terrain.asset 	new file:   Assets/New Terrain.asset.meta 	new file:   Assets/NewLayer 1.terrainlayer 	new file:   Assets/NewLayer 1.terrainlayer.meta 	new file:   Assets/NewLayer 2.terrainlayer 	new file:   Assets/NewLayer 2.terrainlayer.meta 	new file:   Assets/NewLayer.terrainlayer 	new file:   Assets/NewLayer.terrainlayer.meta 	modified:   Assets/Prefabs/FPSController Preston.prefab 	new file:   Assets/SimpleMovement.cs 	new file:   Assets/SimpleMovement.cs.meta 	new file:   Assets/TestScene.unity 	new file:   Assets/TestScene.unity.meta 	new file:   Assets/Tree 1.prefab 	new file:   Assets/Tree 1.prefab.meta 	new file:   Assets/Tree 1_Textures.meta 	new file:   Assets/Tree 1_Textures/diffuse.png 	new file:   Assets/Tree 1_Textures/diffuse.png.meta 	new file:   Assets/Tree 1_Textures/normal_specular.png 	new file:   Assets/Tree 1_Textures/normal_specular.png.meta 	new file:   Assets/Tree 1_Textures/shadow.png 	new file:   Assets/Tree 1_Textures/shadow.png.meta 	new file:   Assets/Tree 1_Textures/translucency_gloss.png 	new file:   Assets/Tree 1_Textures/translucency_gloss.png.meta 	new file:   Assets/Tree 2.prefab 	new file:   Assets/Tree 2.prefab.meta 	new file:   Assets/Tree 2_Textures.meta 	new file:   Assets/Tree 2_Textures/diffuse.png 	new file:   Assets/Tree 2_Textures/diffuse.png.meta 	new file:   Assets/Tree 2_Textures/normal_specular.png 	new file:   Assets/Tree 2_Textures/normal_specular.png.meta 	new file:   Assets/Tree 2_Textures/shadow.png 	new file:   Assets/Tree 2_Textures/shadow.png.meta 	new file:   Assets/Tree 2_Textures/translucency_gloss.png 	new file:   Assets/Tree 2_Textures/translucency_gloss.png.meta 	new file:   Assets/Tree 3.prefab 	new file:   Assets/Tree 3.prefab.meta 	new file:   Assets/Tree 3_Textures.meta 	new file:   Assets/Tree 3_Textures/diffuse.png 	new file:   Assets/Tree 3_Textures/diffuse.png.meta 	new file:   Assets/Tree 3_Textures/normal_specular.png 	new file:   Assets/Tree 3_Textures/normal_specular.png.meta 	new file:   Assets/Tree 3_Textures/shadow.png 	new file:   Assets/Tree 3_Textures/shadow.png.meta 	new file:   Assets/Tree 3_Textures/translucency_gloss.png 	new file:   Assets/Tree 3_Textures/translucency_gloss.png.meta 	new file:   Assets/Tree.prefab 	new file:   Assets/Tree.prefab.meta 	new file:   Assets/Tree_Textures.meta 	new file:   Assets/Tree_Textures/diffuse.png 	new file:   Assets/Tree_Textures/diffuse.png.meta 	new file:   Assets/Tree_Textures/normal_specular.png 	new file:   Assets/Tree_Textures/normal_specular.png.meta 	new file:   Assets/Tree_Textures/shadow.png 	new file:   Assets/Tree_Textures/shadow.png.meta 	new file:   Assets/Tree_Textures/translucency_gloss.png 	new file:   Assets/Tree_Textures/translucency_gloss.png.meta 	modified:   ProjectSettings/InputManager.asset 	modified:   ProjectSettings/PresetManager.asset 	modified:   ProjectSettings/ProjectVersion.txt 	modified:   ProjectSettings/VFXManager.asset

---
+++ Assets/SimpleMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+
+    private CharacterController cc;
+
+    public float moveSpeed;
+    public float rotSpeed;
+    public float sinkSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+	Vector3 move = Vector3.zero;
+
+	// Move character forward
+	float vert = Input.GetAxis("Vertical");
+	if (vert > 0f) // Only move forward.
+	    move += Time.deltaTime * vert * moveSpeed * cc.transform.forward;
+
+	// Lower character to ground
+	if (!cc.isGrounded)
+	    move += Time.deltaTime * sinkSpeed * Vector3.down;
+
+	// Apply character movement
+	cc.Move(move);
+
+	// Rotate character
+	cc.transform.Rotate(Time.deltaTime * Input.GetAxis("Horizontal") * rotSpeed * Vector3.up);
+		
+
+    }
+}
+
