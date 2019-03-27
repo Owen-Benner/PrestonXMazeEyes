a73d76fd853e9a0655c903347e20b9b9e5b48ee5
Owen-Benner <owen.p.benner@gmail.com>
2019-03-27 14:55:56 -0500

    new file:   Assets/DarkGray.mat 	new file:   Assets/DarkGray.mat.meta 	new file:   Assets/FacePlayer.cs 	new file:   Assets/FacePlayer.cs.meta 	new file:   Assets/LightGray.mat 	new file:   Assets/LightGray.mat.meta 	new file:   Assets/Sprite_1.mat 	new file:   Assets/Sprite_1.mat.meta 	new file:   Assets/Sprite_2.mat 	new file:   Assets/Sprite_2.mat.meta 	new file:   Assets/Sprite_3.mat 	new file:   Assets/Sprite_3.mat.meta 	new file:   Assets/Sprite_4.mat 	new file:   Assets/Sprite_4.mat.meta 	new file:   Assets/Sprite_5.mat 	new file:   Assets/Sprite_5.mat.meta 	new file:   Assets/Sprite_6.mat 	new file:   Assets/Sprite_6.mat.meta 	modified:   Assets/XMaze.unity 	new file:   Assets/XMazeHopefullyActuallyFinal.obj 	new file:   Assets/XMazeHopefullyActuallyFinal.obj.meta 	new file:   Assets/XMazeThisTimeActuallyFinal 1.obj 	new file:   Assets/XMazeThisTimeActuallyFinal 1.obj.meta 	new file:   Assets/object_1.png 	new file:   Assets/object_1.png.meta 	new file:   Assets/object_2.png 	new file:   Assets/object_2.png.meta 	new file:   Assets/object_3.png 	new file:   Assets/object_3.png.meta 	new file:   Assets/object_4.png 	new file:   Assets/object_4.png.meta 	new file:   Assets/object_5.png 	new file:   Assets/object_5.png.meta 	new file:   Assets/object_6.png 	new file:   Assets/object_6.png.meta 	modified:   XMaze.x86_64 	modified:   XMaze_Data/GI/level0/11/11699784c916c2a88db49da4cd8e6c89.rsc.sse 	modified:   XMaze_Data/GI/level0/2b/2bb02c116a1ae32a76e10d23d7642b75.rsc.sse 	modified:   XMaze_Data/GI/level0/66/662ffd2e696399de29ba91bd788d8766.rsc.sse 	modified:   XMaze_Data/GI/level0/80/8007e2877e769852e84307a62da9c0e0.caw 	modified:   XMaze_Data/GI/level0/80/8007e2877e769852e84307a62da9c0e0.iws.sse 	modified:   XMaze_Data/GI/level0/80/8007e2877e769852e84307a62da9c0e0.rsc.sse 	modified:   XMaze_Data/GI/level0/d4/d4fa4b6a61cd9855ece82fce9b5e9c0a.rpc.sse 	modified:   XMaze_Data/Managed/Assembly-CSharp-firstpass.dll 	modified:   XMaze_Data/Managed/Assembly-CSharp.dll 	modified:   XMaze_Data/Managed/Unity.Analytics.DataPrivacy.dll 	modified:   XMaze_Data/Managed/Unity.TextMeshPro.dll 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/2.0/Browsers/Compat.browser 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/2.0/DefaultWsdlHelpGenerator.aspx 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/2.0/machine.config 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/2.0/settings.map 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/2.0/web.config 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/4.0/Browsers/Compat.browser 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/4.0/DefaultWsdlHelpGenerator.aspx 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/4.0/machine.config 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/4.0/settings.map 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/4.0/web.config 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/4.5/Browsers/Compat.browser 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/4.5/DefaultWsdlHelpGenerator.aspx 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/4.5/machine.config 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/4.5/settings.map 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/4.5/web.config 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/browscap.ini 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/config 	modified:   XMaze_Data/MonoBleedingEdge/etc/mono/registry/last-btime 	modified:   XMaze_Data/MonoBleedingEdge/x86_64/libMonoPosixHelper.so 	modified:   XMaze_Data/MonoBleedingEdge/x86_64/libmonobdwgc-2.0.so 	modified:   XMaze_Data/Plugins/x86_64/ScreenSelector.so 	modified:   XMaze_Data/Resources/UnityPlayer.png 	modified:   XMaze_Data/Resources/unity default resources 	modified:   XMaze_Data/globalgamemanagers 	modified:   XMaze_Data/level0 	modified:   XMaze_Data/sharedassets0.assets 	modified:   XMaze_Data/sharedassets0.assets.resS

---
+++ Assets/FacePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private float distX;
+    private float distZ;
+
+    private float face;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
